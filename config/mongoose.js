7337a706cc7f7500fbad45260de397eccc18c09c
SparshAgrawa <118478171+SparshAgrawa@users.noreply.github.com>
2023-07-29 14:20:29 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,31 @@
+// const mongoose = require("mongoose");
+
+// const connection = async () => {
+//   try {
+//     return await mongoose.connect(
+//       "mongodb://127.0.0.1:27017/ISSUE_TRACKER_DB   "
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// const db = connection();
+
+// db.then(function (data) {
+//   console.log("connented successfullty");
+// }).catch(function (err) {
+//   console.log("error in connecting", err);
+// });
+
+// module.exports = db;
 const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
     return await mongoose.connect(
-      "mongodb://127.0.0.1:27017/ISSUE_TRACKER_DB   "
+      // "mongodb://127.0.0.1:27017/ISSUE_TRACKER_DB   "
+      "mongodb+srv://sparsh7037:PsTxkrlQatmxNcMN@cluster0.de33ehd.mongodb.net/Hospital_api?retryWrites=true&w=majority"
     );
   } catch (err) {
     console.log(err);
